13a9dde70f9eabfd1c395784cc85e3f193d0ca4a
Jeffrey Burke <jburke0519@gmail.com>
2021-10-27 12:41:32 -0400

    Somewhat working win condition

--- battleship.cpp
+++ battleship.cpp
@@ -25,6 +25,10 @@ int y = 0;
 int player = 0;
 int r = -1;
 bool ships[2][6] = { {true, true, true, true, true, false}, {true,true,true,true,true,true} };
+int hits = 0;
+bool flag = true;
+//hits1 = 0;
+//hits2 = 0;
 HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
 char pieces[5][7] = {
@@ -35,6 +39,9 @@ char pieces[5][7] = {
 	{'<','0','-','0','>'},
 };
 
+/// <summary>
+/// Initializer of battleship
+/// </summary>
 battleship::battleship() {
 	CONSOLE_CURSOR_INFO cursorInfo;
 	GetConsoleCursorInfo(hConsole, &cursorInfo);
@@ -42,6 +49,11 @@ battleship::battleship() {
 	SetConsoleCursorInfo(hConsole, &cursorInfo);
 }
 
+/// <summary>
+/// Changes the cursor position in the console
+/// </summary>
+/// <param name="xx">Sets the x of cursor pos</param>
+/// <param name="yy">Sets the y of cursor pos</param>
 void battleship::setCursorPosition(int xx, int yy) {
 	static const HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 	std::cout.flush();
@@ -49,12 +61,20 @@ void battleship::setCursorPosition(int xx, int yy) {
 	SetConsoleCursorPosition(hOut, coord);
 }
 
+/// <summary>
+/// Highlights text to a passed in color
+/// </summary>
+/// <param name="text"> The text to be highlighted</param>
+/// <param name="color"> Color code to be highlighted</param>
 void battleship::highlight(string text, int color) {
 	SetConsoleTextAttribute(hConsole, color);
 	cout << text;
 	SetConsoleTextAttribute(hConsole, 7);
 }
 
+/// <summary>
+/// Original Series of functions
+/// </summary>
 void battleship::bsmain() {
 	startMenu();
 	while (1) {
@@ -64,8 +84,18 @@ void battleship::bsmain() {
 		player++;
 		saveGame();
 	}
+	while (flag) {
+		startMenu();
+		print();
+		centerShips();
+		functionController();
+		saveGame();
+	}
 }
 
+/// <summary>
+/// This gets run at the start of the program
+/// </summary>
 void battleship::startMenu() {
 	char option;
 	cout << "Would you like to start a new game (n) or load an existing one (l)?\n> ";
@@ -83,6 +113,9 @@ void battleship::startMenu() {
 	}
 }
 
+/// <summary>
+/// Sets every part of grid to a *
+/// </summary>
 void battleship::initGrid() {
 	for (int k = 0; k < 4; k++) {
 		for (int i = 0; i < 10; i++) {
@@ -93,6 +126,9 @@ void battleship::initGrid() {
 	}
 }
 
+/// <summary>
+/// Prints out the current players board
+/// </summary>
 void battleship::print() {
 	system("cls");
 	highlight("\t\tYour ships.", 10);
@@ -124,9 +160,13 @@ void battleship::print() {
 	}
 }
 
+/// <summary>
+/// This directs the users controls to the right function
+/// </summary>
 void battleship::functionController() {
 	bool flag = true;
 	while (flag) {
+	while (hits < 23) { //hits1 < 23 && hits2 < 23
 		int input = _getch();
 		if (input != 224) {
 			if (!(ships[player%2][0] && ships[player%2][1] && ships[player%2][2] && ships[player%2][3] && ships[player%2][4] && ships[player%2][5])) {
@@ -174,8 +214,13 @@ void battleship::functionController() {
 			}
 		}
 	}
+	win();
 }
 
+/// <summary>
+/// Moves the cursor around the grid
+/// </summary>
+/// <param name="m">Takes a character for what dirction you want the cursor to move</param>
 void battleship::move(char m) {
 	setCursorPosition((x * 4) + 10, (y * 2) + 4);
 	if (grid[player % 2][x][y] == '*') {
@@ -208,7 +253,6 @@ void battleship::move(char m) {
 			x++;
 		}
 		break;
-
 	}
 	setCursorPosition((x * 4) + 10, (y * 2) + 4);
 	if (grid[player % 2][x][y] == '*') {
@@ -222,6 +266,9 @@ void battleship::move(char m) {
 		highlight(string(1, grid[player % 2][x][y]), 224);
 }
 
+/// <summary>
+/// Centers the ships between the grids
+/// </summary>
 void battleship::centerShips() {
 	setCursorPosition(54, 9);
 	highlight("Ships", 14);
@@ -237,6 +284,13 @@ void battleship::centerShips() {
 
 }
 
+/// <summary>
+/// Rotates a ship
+/// </summary>
+/// <param name="eks">X position of first part of the ship</param>
+/// <param name="why">Y position of first part of the ship</param>
+/// <param name="piece"></param>
+/// <param name="color">The color for the ship to be highlighted</param>
 void battleship::rotate(int eks, int why, char piece, int color) {
 	if (eks > -1 && eks < 10 && why > -1 && why < 10) {
 		grid[player % 2][eks][why] = piece;
@@ -267,6 +321,10 @@ bool battleship::pathfinder(int eks, int why) {
 	}
 }
 
+/// <summary>
+/// Places the pieces into the board and allows for the user to rotate them
+/// </summary>
+/// <param name="z">Determines the ship numebr</param>
 void battleship::place_pieces(int z) {
 	// Player presses number 1-5, boat spawns, r to rotate 90 degrees
 	setCursorPosition(0, 24);
@@ -361,6 +419,9 @@ void battleship::place_pieces(int z) {
 	}
 }
 
+/// <summary>
+/// Saves the game board to be pulled up later
+/// </summary>
 void battleship::saveGame() {
 	string fName = "save.txt";
 	fstream save;
@@ -378,6 +439,9 @@ void battleship::saveGame() {
 	system("cls");
 }
 
+/// <summary>
+/// Pulls up a previously saved game board
+/// </summary>
 void battleship::loadGame() {
 	string fName = "save.txt";
 	fstream save;
@@ -399,11 +463,35 @@ void battleship::hit() {
 		grid[player % 2][x][y] = 'X';
 		setCursorPosition(x * 4 + 10, y * 2 + 4);
 		highlight(string(1, 'X'), 12);
+		hits++;
 	}
 	else if (grid[player % 2][x][y] == '*') {
 		grid[player % 2][x][y] = 'O';
 		setCursorPosition(x * 4 + 10, y * 2 + 4);
 		highlight(string(1, 'O'), 10);
+	}
+}
+
+// Add a hits1 and hits 2 functionality to this
+void battleship::win() {
+	if (hits > 23) {
+		char loop;
+		system("cls");
+		setCursorPosition(0, 0);
+		do {
+			cout << "Player X won the game, successfully hitting all the opponenets ships!\nWould you like to play again?\ny : yes\nn : no\n> "; // Add player 1 and two
+			cin >> loop;
+			switch (loop) {
+			case 'y':
+				bsmain();
+				break;
+			case 'n':
+				flag = false;
+				break;
+			default:
+				break;
+			}
+		} while (loop != 'y' && loop != 'n');
 	}
 }
 
